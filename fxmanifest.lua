132d8ec73106e92c2c1a7997a3df24a509e0125f
ThunderScripts <52550137+ThunderScripts@users.noreply.github.com>
2022-01-31 20:43:12 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,11 @@ version '1.0.0'
 
 ui_page 'html/index.html'
 
-shared_script 'config.lua'
+shared_scripts {
+    'config.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua', -- Change to the language you want
+}
 
 client_scripts {
     'client/fleeca.lua',
