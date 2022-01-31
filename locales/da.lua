1f6abc518ea27aa0ae95bc9bdaa97ccdb49c20fe
ThunderScripts <52550137+ThunderScripts@users.noreply.github.com>
2022-01-31 20:40:43 +0100

    Create da.lua

---
+++ locales/da.lua
@@ -0,0 +1,43 @@
+local Translations = {
+    error = {
+        canceled = 'Afbrudt',
+        too_strong = 'Det ser ud til at låsen er for kraftig...',
+        missing_item = 'Du mangler en ting...',
+        already_open = 'Ser ud til at banken allerede er åben...',
+        minimum_cops = 'Minimum af %{minCops} politi er der brug for',
+        security_lock = 'Sikkerhedslåsen er aktiv, det er i øjeblikket ikke muligt at åbne døren.',
+        fuse_blowen = 'Det ser ud til, at sikringerne er sprunget.',
+        missing_ignition = 'Du mangler en tændkilde',
+    },
+    success = {
+        successful = 'Succes',
+        fuse_broken = 'Sikringerne er gået i stykker',
+        door_open = 'Døren er åben',
+    },
+    info = {
+        rob_attempt = 'bank røveri startet',
+        thermite_count = 'Thermite går af om %{time}..',
+        alarm_at = 'Alarmen er blevet aktiveret ved %{bank} %{street} (kamera id: %{camid})',
+    },
+    text = {
+        blip_name = '10-90: Bank Røveri',
+        break_safe = 'Bryd boksne op',
+        crack_vault = 'Bryd boksne op',
+    },
+    progress = {
+        breaking_safe = 'Åbner pengeskabet...',
+        connect_device = 'Tilslutter hacking-enheden...',
+        using_card_pacific = 'Validere kort...',
+        using_card_paleto = 'Validere kort...',
+    },
+    banks = {
+        fleeca = 'Fleeca',
+        blaine = 'Blaine County Savings',
+        pacific = 'Pacific Standard Bank',
+    }
+}
+
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
